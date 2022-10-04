c571ecd9588fa74f7d9ab51d7d531e4983522414
AleksaRistovicCT360 <112879866+AleksaRistovicCT360@users.noreply.github.com>
2022-10-04 16:58:01 +0200

    Add files via upload

---
+++ brakovi/src/test/java/com/example/brakovi/BrakoviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brakovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrakoviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
